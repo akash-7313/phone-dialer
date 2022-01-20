10f272c75b39e4391ccfb1f5d315e37b21c45a47
akash-7313 <akashsau79@gmail.com>
2022-01-20 19:53:27 +0600

    second

--- js/phone.js
+++ js/phone.js
@@ -1,23 +1,21 @@
-document.getElementById("type-pin").addEventListener("click", function () {
-  var input = event.target.innerText;
-  var showInput = document.getElementById("show-number");
-  var delInput = document.getElementById("back=arrow");
+document.getElementById("type-num").addEventListener("click", function () {
+  const input = event.target.innerText;
+  const showInput = document.getElementById("show-number");
+  // const delInput = document.getElementById("back=arrow");
+  const previousInput = showInput.value;
     
   if (isNaN(input)) {
     if (input == "*") {
-        var previousInput = showInput.value;
-        var newInput = previousInput + input;
+        const newInput = previousInput + input;
         showInput.value = newInput;
       }
     else if (input == "#") {
-        var previousInput = showInput.value;
-        var newInput = previousInput + input;
+        const newInput = previousInput + input;
         showInput.value = newInput;
     }
   }
   else {
-    var previousInput = showInput.value;
-    var newInput = previousInput + input;
+    const newInput = previousInput + input;
     showInput.value = newInput;
     }
 });
